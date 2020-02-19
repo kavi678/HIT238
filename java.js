c45fcaec37d5e45c3f7af7c0297fd8f715778f09
kavi678 <kavipatel111.kp@gmail.com>
2020-02-20 02:39:29 +0930

    changes
    changes

---
+++ java.js
@@ -0,0 +1,27 @@
+function myFunctionabout() {
+  if (document.getElementById("about_me").style.display == "none"){
+  document.getElementById("about_me").style.display = "block";
+document.getElementById("about1").innerHTML = "Less";}
+  else {
+    document.getElementById("about_me").style.display = "none";
+    document.getElementById("about1").innerHTML = "More";
+  }
+}
+function myFunctioneducation() {
+  if (document.getElementById("education").style.display == "none"){
+  document.getElementById("education").style.display = "block";
+document.getElementById("edu1").innerHTML = "Less";}
+  else {
+    document.getElementById("education").style.display = "none";
+    document.getElementById("edu1").innerHTML = "More";
+  }
+}
+function myFunctionempl() {
+  if (document.getElementById("empl").style.display == "none"){
+  document.getElementById("empl").style.display = "block";
+document.getElementById("empl1").innerHTML = "Less";}
+  else {
+    document.getElementById("empl").style.display = "none";
+    document.getElementById("empl1").innerHTML = "More";
+  }
+}
