1e26a4d9aa53a594dd7379f31cbb3436a8ae5a61
kavi678 <kavipatel111.kp@gmail.com>
2020-03-03 16:42:08 +0930

    xyz

---
+++ info.php
@@ -0,0 +1,12 @@
+
+<html>
+  <head>
+    
+    <title>Information</title>
+  </head>
+  <body>
+    <?php
+    echo "<p>Done yrr</>";
+     ?>
+  </body>
+</html>
--- java.js
+++ java.js
@@ -25,3 +25,30 @@ document.getElementById("empl1").innerHTML = "Less";}
     document.getElementById("empl1").innerHTML = "More";
   }
 }
+function myMove() {
+  var elem = document.getElementById("animate");
+  var elem1 = document.getElementById("animate1");
+  var elem2 = document.getElementById("animate2");
+  var pos = 0;
+  var d1 = -45;
+  var d2 = 45;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      d1++;
+      d2++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+      elem1.style.top = pos + 'px';
+      elem1.style.left = pos + 'px';
+      elem2.style.top = pos + 'px';
+      elem2.style.left = pos + 'px';
+      elem1.style.transform = 'skewX'+'('+d1+'deg'+')';
+      elem2.style.transform = 'skewX'+'('+d2+'deg'+')';
+    }
+
+  }
+}
